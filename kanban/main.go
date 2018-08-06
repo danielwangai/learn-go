9632577510ac87a32ba2bde71f55870be6c1653a
Daniel Wangai <mainad57@gmail.com>
2018-08-06 14:03:51 +0300

    add error handler method

--- kanban/main.go
+++ kanban/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "learn-go/kanban/cmd"
+import (
+	"fmt"
+	"learn-go/kanban/cmd"
+	"learn-go/kanban/db"
+	"os"
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "task.db")
+	errorHandler(db.Init(dbPath))
+	errorHandler(cmd.RootCmd.Execute())
+}
+
+func errorHandler(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
